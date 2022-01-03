8e066aeec37ad3c77ff56550d797aef62fc66d39
Lyly Louanghaksaphone <llouanghaksaphone@automotivemastermind.com>
2022-01-03 17:49:29 -0500

    feat(google): upgrade provider version to support 0.14

--- main.tf
+++ main.tf
@@ -90,7 +90,7 @@ resource "google_compute_firewall" "nat-gateway" {
 }
 
 module "nat-gateway" {
-  source             = "github.com/automotivemastermind/terraform-google-managed-instance-group?ref=2.0.0"
+  source             = "github.com/leeleelou/terraform-google-managed-instance-group?ref=92306"
   name               = local.name
   project            = var.project
   region             = var.region
